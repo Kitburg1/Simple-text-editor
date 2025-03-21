1b41d87e70bb4d8fbdd95da0fea0306a15d96df4
Kitburg1 <sss438248@gmail.com>
2025-03-21 16:17:51 +0300

    My first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class MyProgram{
+    static async Task Main(string[] args){
+        if(args.Length==0){
+            Console.WriteLine("Введите имя файла!");
+            Environment.Exit(0);
+        }
+        Console.WriteLine("Добро Пожаловать в мой текстовый редактор!");
+        Console.WriteLine("");
+        string? path = args[0];
+        Console.Write(">> ");
+        string? text = Console.ReadLine();
+        using (StreamWriter writer = new StreamWriter(path, true)){
+            await writer.WriteLineAsync(text);
+        }
+    }
+}
